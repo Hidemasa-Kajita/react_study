4bf481ad8124e92c1a11ed851ab87c1e47dd7319
梶田英完 <hidemasa_kajita@respawn.jp>
2022-01-08 22:52:51 +0900

    feat: count app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>count app</p>
+      <p>count: {count}</p>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      {count !== 0 && <button onClick={reset}>reset</button>}
+    </>
+  )
 }
 
-export default App;
+export default App
