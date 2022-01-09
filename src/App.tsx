6d7cd6f3cb841a6aa2f79239f96d621fc94a4814
梶田英完 <hidemasa_kajita@respawn.jp>
2022-01-09 15:12:00 +0900

    feat: todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useEffect, useState, VFC } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const STATUS = {
+  PENDING: 'pending',
+  DONE: 'done',
+  PROGRESS: 'progress',
+  EXPIRED: 'expired',
+} as const
+
+type Status = typeof STATUS[keyof typeof STATUS]
+
+type TodoStatus = {
+  id: number
+  name: Status
 }
 
-export default App;
+type Todo = {
+  id: number
+  name: string
+  status: Status
+}
+
+const App: VFC = () => {
+  const [todo, setTodo] = useState<Todo[]>([])
+  const [statuses, setStatuses] = useState<TodoStatus[]>([])
+  const [updateStatus, setUpdateStatus] = useState<Status>(STATUS.PENDING)
+  const [newTask, setNewTask] = useState('')
+
+  const getTodo = async () => {
+    const response = await axios.get<Todo[]>('http://localhost:3001/todo')
+
+    setTodo(() => response.data)
+  }
+
+  const getStatuses = async () => {
+    const response = await axios.get<TodoStatus[]>(
+      'http://localhost:3001/statuses',
+    )
+    setStatuses(() => response.data)
+  }
+
+  useEffect(() => {
+    getStatuses()
+    getTodo()
+  }, [])
+
+  const handleChangeNewTask = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTask(() => e.target.value)
+  }
+
+  const addTask = async () => {
+    const response = await axios.post<Todo>('http://localhost:3001/todo', {
+      name: newTask,
+      status: STATUS.PENDING,
+    })
+
+    setTodo((prev) => [...prev, response.data])
+  }
+
+  const changeStatus = async (task: Todo) => {
+    await axios.put<Todo>(`http://localhost:3001/todo/${task.id}`, {
+      name: task.name,
+      status: updateStatus,
+    })
+
+    getTodo()
+  }
+
+  const handleChangeStatus = (e: ChangeEvent<HTMLSelectElement>) => {
+    setUpdateStatus(() => e.target.value as Status)
+  }
+
+  const deleteTask = async (id: number) => {
+    await axios.delete<Todo>(`http://localhost:3001/todo/${id}`)
+
+    getTodo()
+  }
+
+  return (
+    <div>
+      <h1>todo app</h1>
+      <div>
+        <h2>新規タスク追加</h2>
+        <input value={newTask} onChange={handleChangeNewTask} />
+        <button onClick={addTask}>追加</button>
+      </div>
+      <div>
+        <h2>タスクリスト</h2>
+        変更ステータス:
+        <select onChange={handleChangeStatus}>
+          {statuses.map((status) => (
+            <option key={status.id} value={status.name}>
+              {status.name}
+            </option>
+          ))}
+        </select>
+        <ul>
+          {todo.map((task) => (
+            <li key={task.id}>
+              {task.id}: {task.name}: {task.status}:
+              <button onClick={() => changeStatus(task)}>更新</button>
+              <button onClick={() => deleteTask(task.id)}>削除</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
