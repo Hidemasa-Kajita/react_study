9d0f5be362204f6e7c98f8519b0ff975e2364329
梶田英完 <hidemasa_kajita@respawn.jp>
2022-02-06 15:22:08 +0900

    comment

--- go_gin/go/src/request/task.go
+++ go_gin/go/src/request/task.go
@@ -1,7 +1,9 @@
 package request
 
 type Task struct {
-	Name                  string  `json:"name" binding:"required,max=255,min=1"`
+	Name string `json:"name" binding:"required,max=255,min=1"`
+	// https://github.com/gin-gonic/gin/issues/1193
+	// bag っぽいので、バリデーションは一旦スルーして、string で定義
 	StartDate             *string `json:"start_date"`
 	EndDate               *string `json:"end_date"`
 	ImplementationHours   *int    `json:"implementation_hours"`
